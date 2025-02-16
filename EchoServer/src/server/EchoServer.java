e6283e465facce412ce8df8121ef5a9c987a4dbc
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-02-16 14:01:08 +0600

    a new ClientHandler is created and launched in a separate thread

--- EchoServer/src/server/EchoServer.java
+++ EchoServer/src/server/EchoServer.java
@@ -22,7 +22,7 @@ public class EchoServer {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 System.out.println("Клиент подключился.");
-                handle(socket);
+                new Thread(new ClientHandler(socket)).run();
 
             }
 
@@ -32,59 +32,4 @@ public class EchoServer {
         }
     }
 
-    private static void handle(Socket socket) {
-
-        System.out.printf("Подключен клиент: %s%n", socket);
-
-        try (socket;
-
-             Scanner reader = getReader(socket);
-             PrintWriter writer = getWriter(socket)) {
-            sendResponse("Привет " + socket, writer);
-
-            while (true) {
-
-                String message = reader.nextLine();
-                if (isEmptyMsg(message) || isQuitMsg(message)) {
-                    break;
-                }
-                sendResponse(message.toUpperCase(), writer);
-            }
-        } catch (NoSuchElementException ex) {
-            System.out.println("Клиент закрыл соединение!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.printf("Клиент отключен: %s%n", socket);
-
-    }
-
-    private static PrintWriter getWriter(Socket socket) throws IOException {
-        OutputStream stream = socket.getOutputStream();
-        return new PrintWriter(stream);
-
-    }
-
-    private static Scanner getReader(Socket socket) throws IOException {
-        InputStream stream = socket.getInputStream();
-        InputStreamReader input = new InputStreamReader(stream, "UTF-8");
-        return new Scanner(input);
-
-    }
-
-    private static boolean isQuitMsg(String message) {
-        return "bye".equals(message.toLowerCase());
-    }
-
-    private static boolean isEmptyMsg(String message) {
-        return message == null || message.isBlank();
-    }
-
-    private static void sendResponse(String response, Writer writer) throws IOException {
-        writer.write(response);
-        writer.write(System.lineSeparator());
-        writer.flush();
-
-    }
 }
