0508cadf8dbfac1c604d72bcb9c58e3b1a365929
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-02-16 18:11:42 +0600

    Implemented: 1)Storage of connected users 2)Accepting new connections 3)Sending messages to all users except the sender 4)Deleting disconnected users

--- EchoServer/src/server/EchoServer.java
+++ EchoServer/src/server/EchoServer.java
@@ -1,13 +1,14 @@
 package server;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class EchoServer {
     private final int port;
+    private static final Set<ClientHandler> clients = ConcurrentHashMap.newKeySet();
 
     private EchoServer(int port) {
         this.port = port;
@@ -19,17 +20,30 @@ public class EchoServer {
 
     public void run() {
         try (ServerSocket server = new ServerSocket(port)) {
+            System.out.println("Сервер запущен на порту " + port);
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 System.out.println("Клиент подключился.");
-                new Thread(new ClientHandler(socket)).run();
-
+                ClientHandler clientHandler = new ClientHandler(socket, this);
+                clients.add(clientHandler);
+                new Thread(clientHandler).run();
             }
-
         } catch (IOException e) {
             System.out.printf("Ошибка: порт %d занят.%n", port);
             e.printStackTrace();
         }
     }
 
+    public void broadcastMessage(String sender, String message, ClientHandler senderClient) {
+        for (ClientHandler client : clients) {
+            if (client != senderClient) {
+                client.sendMessage(sender + ": " + message);
+            }
+        }
+    }
+
+    public void removeClient(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+        System.out.println("Клиент отключен: " + clientHandler.getUsername());
+    }
 }
